5d83e54288ed2d7b0efae9bde66556050f0654b3
repsukka <81526112+repsukka@users.noreply.github.com>
2021-04-29 14:02:28 +0300

    asfdg

--- TerrainOpetus/Assets/FireControl.cs
+++ TerrainOpetus/Assets/FireControl.cs
@@ -1,24 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
-public class Uusi : MonoBehaviour
+public class FireControl : MonoBehaviour
 {
-    public Transform target;
-    NavMeshAgent agent;
-
     // Start is called before the first frame update
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
 
-        agent.SetDestination(target.position);
-
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.tag == "Player")
+            GetComponent<ParticleSystem>().Play(); 
     }
 }
